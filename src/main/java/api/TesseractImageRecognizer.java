a5c1b53e7496e7665ce9343d8f8994bda3a95c0d
chucycc <mf27262000@gmail.com>
2014-11-07 00:13:47 -0500

    tesseract

--- src/main/java/api/TesseractImageRecognizer.java
+++ src/main/java/api/TesseractImageRecognizer.java
@@ -1,12 +1,25 @@
 package api;
 
-import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 
 public class TesseractImageRecognizer implements ImageRecognizer {
+	private Tesseract tesseract;
 
-	public String getString(Image segment) {
-		// TODO Auto-generated method stub
-		return null;
+	public TesseractImageRecognizer() {
+		tesseract = Tesseract.getInstance();
+	}
+
+	public String getString(BufferedImage segment){
+		String str = null;
+		try {
+			str = tesseract.doOCR(segment);
+		} catch (TesseractException e) {
+			e.printStackTrace();
+		}
+		return str;
 	}
 
 }
