4c27aa36014efb622c14e7dfc3f4abbb315fbd47
Xingyu Yan <yxypriv@gmail.com>
2015-02-02 11:33:10 -0500

    finished selection

--- src/main/java/gui/SelectionPanel.java
+++ src/main/java/gui/SelectionPanel.java
@@ -9,6 +9,7 @@ import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JPanel;
 
@@ -18,31 +19,56 @@ public class SelectionPanel extends JPanel {
 
 	SelectionPanel thisObj;
 	Rectangle rect;
-	Point[] dragPoint;
-	boolean dragging;
+	Rectangle[] resizingRect;
+
+	Point _mouseStart;
+	Rectangle _rectStart;
+
+	int resizingDirection = -1;
+
+	enum Status {
+		initialing, resizing, relocating, silence
+	}
+
+	Status status;
 
 	public SelectionPanel() {
 		thisObj = this;
-		dragPoint = new Point[2];
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
-				dragging = true;
-				if (null == rect) {
+				status = Status.silence;
+				if (null == rect) { // empty
 					rect = new Rectangle(e.getX(), e.getY(), 0, 0);
-				} else if (rect.contains(e.getPoint())) {
-					dragPoint[0] = (Point) e.getPoint().clone();
-					dragPoint[1] = (Point) rect.getLocation().clone();
-				} else {
-					dragging = false;
+					status = Status.initialing;
+				} else { // exists
+					_mouseStart = (Point) e.getPoint().clone();
+					_rectStart = (Rectangle) rect.clone();
+
+					for (int i = 0; i < resizingRect.length; i++) {
+						if (resizingRect[i].contains(e.getX(), e.getY())) {
+							status = Status.resizing;
+							resizingDirection = i;
+							break;
+						}
+					}
+					if (status != Status.resizing)
+						if (rect.contains(e.getPoint())) {
+							status = Status.relocating;
+						} else
+							status = Status.silence;
 				}
 				super.mouseDragged(e);
 			}
 
 			@Override
 			public void mouseReleased(MouseEvent e) {
-				dragging = false;
-				dragPoint[0] = null;
+				status = Status.silence;
+				resizingRect = new Rectangle[4];
+				for (int i = 0; i < resizingRect.length; i++)
+					resizingRect[i] = new Rectangle();
+				updateResizingRect();
+				repaint();
 				super.mouseReleased(e);
 			}
 		});
@@ -50,15 +76,36 @@ public class SelectionPanel extends JPanel {
 		addMouseMotionListener(new MouseMotionAdapter() {
 			@Override
 			public void mouseDragged(MouseEvent e) {
-				if (null != rect && dragging) {
-					if (null == dragPoint[0])
-						rect.setSize((int) (e.getX() - rect.getX()), (int) (e.getY() - rect.getY()));
-					else {
-						rect.setBounds((int) (dragPoint[1].getX() + (e.getX() - dragPoint[0].getX())),//
-								(int) (dragPoint[1].getY() + (e.getY() - dragPoint[0].getY())), //
-								(int) rect.getWidth(), (int) rect.getHeight());
+				if (status == Status.initialing) {
+					rect.setSize((int) (e.getX() - rect.getX()), (int) (e.getY() - rect.getY()));
+				} else {
+					int mouseDeltaX = (int) e.getX() - (int) _mouseStart.getX();
+					int mouseDeltaY = (int) e.getY() - (int) _mouseStart.getY();
+					int rbX = (int) _rectStart.getX() + (int) _rectStart.getWidth();
+					int rbY = (int) _rectStart.getY() + (int) _rectStart.getHeight();
+					if (status == Status.resizing) {
+						if (resizingDirection == 0) {
+							rect.setBounds((int) (_rectStart.getX() + mouseDeltaX),//
+									(int) (_rectStart.getY() + mouseDeltaY), //
+									(int) (rbX - e.getX()), (int) (rbY - e.getY()));
+						} else if (resizingDirection == 1) {
+							rect.setBounds((int) (_rectStart.getX()),//
+									(int) (_rectStart.getY() + mouseDeltaY), //
+									(int) (e.getX() - rect.getX()), (int) (rbY - e.getY()));
+						} else if (resizingDirection == 2) {
+							rect.setBounds((int) (_rectStart.getX() + mouseDeltaX),//
+									(int) (_rectStart.getY()), //
+									(int) (rbX - e.getX()), (int) (e.getY() - rect.getY()));
+						} else {
+							rect.setSize((int) (e.getX() - rect.getX()), (int) (e.getY() - rect.getY()));
+						}
 
+					} else if (status == Status.relocating) {
+						rect.setBounds((int) (_rectStart.getX() + (mouseDeltaX)),//
+								(int) (_rectStart.getY() + (mouseDeltaY)), //
+								(int) rect.getWidth(), (int) rect.getHeight());
 					}
+					updateResizingRect();
 				}
 				super.mouseDragged(e);
 				repaint();
@@ -66,14 +113,40 @@ public class SelectionPanel extends JPanel {
 
 			@Override
 			public void mouseMoved(MouseEvent e) {
-
-				if (null != rect && rect.contains(e.getX(), e.getY()))
-					thisObj.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-				else
+				final int[] resizingCursorIndex = new int[] { Cursor.NW_RESIZE_CURSOR, Cursor.NE_RESIZE_CURSOR, Cursor.SW_RESIZE_CURSOR, Cursor.SE_RESIZE_CURSOR };
+				if (null != rect) {
+					boolean cursorResizing = false;
+					if (status == Status.silence) {
+						for (int i = 0; i < resizingRect.length; i++) {
+							if (resizingRect[i].contains(e.getX(), e.getY())) {
+								thisObj.setCursor(Cursor.getPredefinedCursor(resizingCursorIndex[i]));
+								cursorResizing = true;
+								break;
+							}
+						}
+						if (!cursorResizing)
+							if (rect.contains(e.getX(), e.getY()))
+								thisObj.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+							else
+								thisObj.setCursor(Cursor.getDefaultCursor());
+					}
+				} else
 					thisObj.setCursor(Cursor.getDefaultCursor());
 				super.mouseMoved(e);
 			}
 		});
+	}
+
+	public void updateResizingRect() {
+		final int resizeSize = 8;
+		resizingRect[0].setBounds((int) (rect.getX() - resizeSize / 2),//
+				(int) (rect.getY() - resizeSize / 2), resizeSize, resizeSize);
+		resizingRect[1].setBounds((int) (rect.getX() + rect.getWidth() - resizeSize / 2),//
+				(int) (rect.getY() - resizeSize / 2), resizeSize, resizeSize);
+		resizingRect[2].setBounds((int) (rect.getX() - resizeSize / 2),//
+				(int) (rect.getY() + rect.getHeight() - resizeSize / 2), resizeSize, resizeSize);
+		resizingRect[3].setBounds((int) (rect.getX() + rect.getWidth() - resizeSize / 2),//
+				(int) (rect.getY() + rect.getHeight() - resizeSize / 2), resizeSize, resizeSize);
 	}
 
 	@Override
@@ -83,6 +156,13 @@ public class SelectionPanel extends JPanel {
 		g2.setColor(Color.red);
 		if (null != rect) {
 			g2.draw(rect);
+		}
+		if (null != resizingRect) {
+			g2.setColor(Color.gray);
+			for (Rectangle rr : resizingRect) {
+				g2.clearRect((int) rr.getX(), (int) rr.getY(), (int) rr.getWidth(), (int) rr.getHeight());
+				g2.draw(rr);
+			}
 		}
 	}
 }
